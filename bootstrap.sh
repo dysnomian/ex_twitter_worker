ee2744fb8c658c4e8bd004922225bb28c6014de0
Liss McCabe <liss@eristiccode.com>
2015-03-19 21:36:08 -0500

    Tweak bootstrap.sh again

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#####
+# Elixir bootstrap
+#####
+
 # System stuff
 echo "Adding ppas and other pre-update packages..."
 sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
@@ -10,24 +14,29 @@ echo "Updating apt..."
 sudo apt-get update
 
 echo "Installing system packages."
-sudo apt-get install -y git vim tmux zsh rbenv rcm redis-server elixir
+sudo apt-get install -y git vim tmux zsh rcm redis-server elixir
 
 # Ruby environment
 echo "Setting up Ruby installer."
-git clone https://github.com/sstephenson/ruby-build.git $HOME/ruby-build
-sudo $HOME/ruby-build/install.sh
+mkdir $HOME/bin
+cd $HOME/bin
+curl -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
+curl -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+tar -xzvf ruby-install-0.5.0.tar.gz
+cd ruby-install
+sudo make install
+cd chruby-0.3.9/
+sudo make install
+sudo ./scripts/setup.sh
 
-# User files
-echo "Entering userspace."
-mkdir $HOME/src
-rbenv install 2.2.1
-gem install bundler mustache
-
-# Install dotfiles
+# Install dotfiles (assumes Ruby for rcm)
 echo "Installing dotfiles..."
+gem install bundler mustache
 git clone https://github.com/dysnomian/polka.git $HOME/src/polka
 cd $HOME/src/polka
 yes | env RCRC=$HOME/src/polka/rcrc rcup
+sudo chsh -s $(which zsh) vagrant
 
 # Install project
 echo "Installing project..."
@@ -36,5 +45,7 @@ git clone https://github.com/dysnomian/ex_twitter_worker.git $HOME/src/ex_twitte
 # Configure project
 echo "Configuring project..."
 cd $HOME/src/ex_twitter_worker
-yes 'Y' | mix deps.get
+mix deps.get -y
 redis-server &
+
+echo "All done! Connect with vagrant ssh."
